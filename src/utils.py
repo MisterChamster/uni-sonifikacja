128d2dd2931c60fde0626f17ff5b882f44956ce1
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-01-05 19:57:36 +0100

    Name change

--- src/utils.py
+++ src/utils.py
@@ -16,7 +16,7 @@ class Utils():
 
 
     @staticmethod
-    def fix_value_in_settingsjson(adress: str, json_key: str, json_val: bool|str|int|float) -> None:
+    def save_value_to_settings_FIX(adress: str, json_key: str, json_val: bool|str|int|float) -> None:
         with open(adress) as f:
             config = json.load(f)
 
