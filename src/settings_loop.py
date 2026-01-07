4792d553bed176c5f190991d1ef097d88e8f0f61
MisterChamster <s.duda03@gmail.com>
2026-01-07 21:14:53 +0100

    Readability update

--- src/settings_loop.py
+++ src/settings_loop.py
@@ -3,27 +3,31 @@ from src.utils import Utils
 
 
 
-def settings_loop():
+def settings_loop() -> None:
     while True:
         asker_settings: str = Askers.ask_settings()
 
         if asker_settings == "change_cutting_setting_paa":
-            Utils.change_setting_to_opposite("src/settings.json",
-                                                "CUT_REMAINDER_STRING_PAA",
-                                                True)
+            Utils.change_setting_to_opposite(
+                "src/settings.json",
+                "CUT_REMAINDER_STRING_PAA",
+                True)
             print()
         elif asker_settings == "change_cutting_setting_dwelltimes":
-            Utils.change_setting_to_opposite("src/settings.json",
-                                                "CUT_REMAINDER_STRING_DWELLTIMES",
-                                                True)
+            Utils.change_setting_to_opposite(
+                "src/settings.json",
+                "CUT_REMAINDER_STRING_DWELLTIMES",
+                True)
         elif asker_settings == "change_segmenting_setting_paa":
-            Utils.change_setting_to_opposite("src/settings.json",
-                                                "SEGMENTING_PAA",
-                                                "size")
+            Utils.change_setting_to_opposite(
+                "src/settings.json",
+                "SEGMENTING_PAA",
+                "size")
         elif asker_settings == "change_segmenting_setting_dwelltimes":
-            Utils.change_setting_to_opposite("src/settings.json",
-                                                "SEGMENTING_DWELLTIMES",
-                                                "size")
+            Utils.change_setting_to_opposite(
+                "src/settings.json",
+                "SEGMENTING_DWELLTIMES",
+                "size")
         elif asker_settings == "exit":
             return
 
